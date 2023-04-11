1abe28f8f5da5cdc0e8a082bacd3c1870853f315
Nikita <amstaffix@gmail.com>
2023-04-12 00:33:27 +0700

    Added workaround to add necessary functions. It will be helpful in environments, where not all necessary bit functions exists

--- lockbox/util/bit.lua
+++ lockbox/util/bit.lua
@@ -22,4 +22,23 @@ if e.ror and not e.rrotate then
     e.rrotate = e.ror
 end
 
+-- Workaround to support incomplete bit operations set
+if not e.ror and not e.rrotate then
+    local ror = function(b, n)
+        return e.bor(e.rshift(b, n), e.lshift(b, 32 - n))
+    end
+
+    e.ror = ror
+    e.rrotate = ror
+end
+
+if not e.rol and not e.lrotate then
+    local ror = function(b, n)
+        return e.bor(e.lshift(b, n), e.rshift(b, 32 - n))
+    end
+
+    e.rol = ror
+    e.lrotate = ror
+end
+
 return e
